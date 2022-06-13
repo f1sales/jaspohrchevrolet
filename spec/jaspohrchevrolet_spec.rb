e22988e7231a63572c743a080c0809d4ffd4ea16
Nathanael Vieira <nathanael@f1sales.com.br>
2022-06-13 17:29:27 -0300

    Initial preparations were made

--- spec/jaspohrchevrolet_spec.rb
+++ spec/jaspohrchevrolet_spec.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 RSpec.describe Jaspohrchevrolet do
   it "has a version number" do
     expect(Jaspohrchevrolet::VERSION).not_to be nil
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(true).to eq(true)
   end
 end
