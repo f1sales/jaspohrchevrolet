94a663254510b7846d55e0556104fc0109c561b5
Nathanael Vieira <nathanael@f1sales.com.br>
2022-06-13 17:07:20 -0300

    Initial commit

---
+++ lib/jaspohrchevrolet.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jaspohrchevrolet/version"
+
+module Jaspohrchevrolet
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jaspohrchevrolet_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Jaspohrchevrolet do
+  it "has a version number" do
+    expect(Jaspohrchevrolet::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
